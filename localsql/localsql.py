0c6b7e73fcdcdc36d5b0b4991599368ec85c6322
Andy Kipp <anki-code@users.noreply.github.com>
2024-01-05 14:50:37 +0100

    Update localsql.py

--- localsql/localsql.py
+++ localsql/localsql.py
@@ -43,7 +43,7 @@ class LocalSQL():
         pd.set_option('display.width', None)
         pd.set_option('display.max_columns', 1000)
         pd.set_option('display.max_rows', 1000)
-        pd.set_option('precision', 4)
+        pd.set_option('display.precision', 4)
         #pd.options.display.float_format = '{:,}'.format
         #pd.set_option('display.max_colwidth', 100)
 
