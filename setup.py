a84c88facb30d91a0983b4927192b7b77a3b6f95
Andy Kipp <anki-code@users.noreply.github.com>
2024-01-05 15:06:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     python_requires='>=3.6',
-    install_requires=['pandas', 'pandasql', 'openpyxl', 'prompt_toolkit', 'pygments', 'tableprint', 'argcomplete'],
+    install_requires=['pandas', 'pandasql', 'openpyxl', 'prompt_toolkit', 'pygments', 'tableprint', 'argcomplete', 'sqlalchemy<2'],
     scripts=['localsql/localsql'],
     packages=setuptools.find_packages(),
     package_data={'localsql': ['*.py']},
